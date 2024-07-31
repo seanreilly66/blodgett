fd72468e67c81cdca5f4dbb2855ead7fc45d8bc0
Sean Reilly <sean.reilly66@gmail.com>
2024-07-31 15:40:39 -0700

    Add laz to las conversion, update output to file, beginning of tree delineation

--- R/las_ctg_extract.R
+++ R/las_ctg_extract.R
@@ -49,14 +49,70 @@ shp_file <- 'data/gis/BlodgettFFS_UnitBoundaries/FFS_COMPS_only_Alb83.shp'
 file_skeleton <- 'data/las/als/bldgt_als_{id}.las'
 
 
-ctg <- list.files(
+
+# Convert laz to las
+
+laz_files <- list.files(
   path = las_folder,
-  pattern = file_ext,
+  pattern = '.laz$',
   full.names = T
-) %>%
+)
+
+# las_files <- list.files(
+#   path = las_folder,
+#   pattern = '.las$',
+#   full.names = T
+# ) 
+# 
+# prog <- laz_files[!(laz_files %in% str_replace(las_files, 'las$', 'laz'))] %>%
+#   sample()
+
+
+
+for (i in prog) {
+  
+  output_file <- str_replace(i, 'laz$', 'las')
+  
+  readLAS(i) %>%
+    writeLAS(output_file)
+  
+}
+
+
+
+ctg <- list.files(
+    path = las_folder,
+    pattern = '.las$',
+    full.names = T
+  ) %>%
   readLAScatalog()
 
 boundary <- st_read(shp_file) %>%
   st_transform(st_crs(ctg))
 
+opt_output_files(ctg) <- 'data/las/als/bldgt_als18_{id}'
+
 x <- clip_roi(ctg, boundary)
+
+
+#
+
+als <- list.files(path = 'data/las/als',
+                  pattern = '.las$',
+                  full.names = T)
+
+i = 1
+
+x = readLAS(als[i]) %>%
+  filter_duplicates() %>%
+  filter_poi(Classification != 7, Classification != 18) %>%
+  normalize_height(tin()) 
+
+ttop <- locate_trees(x, lmf(ws = 5))
+
+p <- plot(x, bg = "white", size = 4)
+add_treetops3d(p, ttop)
+
+# Look at variable window size by height
+
+
