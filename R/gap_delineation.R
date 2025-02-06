d05213adfdcafe6fe162ef9f9dfb94c675e4e825
Sean Reilly <sean.reilly66@gmail.com>
2025-02-06 13:40:45 -0800

    Initial commit

---
+++ R/gap_delineation.R
@@ -0,0 +1,120 @@
+# ==============================================================================
+#
+# Forest gap delineation
+#
+# ==============================================================================
+#
+# Author: Sean Reilly, sean.reilly66@gmail.com
+#
+# Created: 12 Dec 2024
+# Last commit: 20 Jan 2025
+#
+# Status: Needs documentation
+#
+# ==============================================================================
+#
+# Description:
+#
+# ==============================================================================
+#
+# User inputs:
+#
+# ==============================================================================
+#
+# Package dependencies:
+#
+# ==============================================================================
+#
+# Known problems:
+#
+# ==============================================================================
+
+library(tidyverse)
+library(terra)
+library(ForestGapR)
+# library(viridis)
+library(spatstat)
+library(sf)
+library(glue)
+
+# ==============================================================================
+# ================================= User inputs ================================
+# ==============================================================================
+
+chm_folder <- 'data/chm'
+chm_id <- 'bldgt_als22_.+_chm_p2r_025\\.tif$'
+
+study_shp <- 'data/gis/ffs_10m_buffer.shp'
+
+gap_output <- 'data/canopy_gaps/bldgt_als22_c{comp}_t{treatment}_{type}_thresh{threshold}.{ext}'
+
+threshold_heights <- c(1, 3, 5, 10)
+size <- c(1, 10^10)
+
+
+# ==============================================================================
+# ============================= Gap identification =============================
+# ==============================================================================
+
+chm_files <- list.files(chm_folder, pattern = chm_id, full.names = T)
+
+output_files <- c()
+i = 1
+
+for (chm_i in chm_files) {
+  
+  message('Processing: ', i, ' of ', length(chm_files))
+  
+  chm <- rast(chm_i)
+  
+  comp <- chm_i %>%
+    str_extract('(?<=_c)[:digit:]+')
+  
+  treatment <- str_extract(chm_i, '(?<=_t)[:alpha:]+')
+  
+  analysis_window <- study_shp %>%
+    st_read(quiet = T) %>%
+    st_transform(st_crs(chm)) %>%
+    filter(COMP == comp)
+  
+  chm <- mask(chm, analysis_window)
+  
+  # Generate gaps
+  
+  for (threshold in threshold_heights) {
+    
+    message('Threshold: ', threshold)
+    
+    gap_rast <- ForestGapR::getForestGaps(chm_layer = chm,
+                              threshold = threshold,
+                              size = size)
+    
+    gap_stats <- ForestGapR::GapStats(gap_layer = gap_rast, chm_layer = chm)
+    
+    gap_shp <- ForestGapR::GapSPDF(gap_rast) %>%
+      merge(gap_stats, by = 'gap_id') %>%
+      st_as_sf()
+    
+    shp_output <- glue(gap_output, type = 'gapstatshp', ext = 'shp')
+    rast_output <- glue(gap_output, type = 'gaprast', ext = 'tif')
+    
+    writeRaster(gap_rast, 
+                filename = rast_output,
+                overwrite = T)
+    
+    st_write(gap_shp,
+             dsn = shp_output,
+             quiet = T)
+    
+    output_files <- c(output_files, shp_output, rast_output)
+    
+  }
+  
+  i <- i + 1
+  
+}
+
+
+# ==============================================================================
+# ==============================================================================
+# ==============================================================================
